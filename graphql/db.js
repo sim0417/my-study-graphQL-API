cb4329d77568fe73f4da42f64ed08101a1ad2d90
Eon Jo <34697086+sim0417@users.noreply.github.com>
2020-11-13 19:24:35 +0900

    movie API dosen't work :(

--- graphql/db.js
+++ graphql/db.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const BASE_URL = "https://yts-proxy.now.sh/";
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const BASE_URL = `${PROXY_URL}https://yts.am/api/v2/`;
+const BASE_URL = `${PROXY_URL}https://yts.mx/api/v2/`;
+
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
